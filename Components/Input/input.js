80dcee1b394982016d8fbb56f73eddd06ddc810d
Devananth <devananthnkl@gmail.com>
2022-02-08 10:24:26 +0530

    Fix : removed console.log

--- Components/Input/input.js
+++ Components/Input/input.js
@@ -1,7 +1,6 @@
 const forms = document.querySelectorAll(".form");
 
 forms.forEach((form) => {
-  console.log(form);
   form.addEventListener("submit", (e) => {
     if (!form.checkValidity()) {
       e.preventDefault();
